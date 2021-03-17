120f29fa0496a3ab9e2e31b691a6113baf03d5db
mbtgy <64004726+mbtgy@users.noreply.github.com>
2021-03-17 16:30:19 +0100

    Update Dnu.R

--- R/Dnu.R
+++ R/Dnu.R
@@ -7,4 +7,5 @@
 #'
 #' @importFrom fAsianOptions whittakerW
 #' @export
-Dnu=function(z,nu){2^(1/4+nu/2)*z^(-1/2)*whittakerW(z^2/2, kappa=1/4+nu/2,mu=-1/4)}
+Dnu=function(z,nu){2^(nu/2)*exp(-z^2/4)*(sqrt(pi)/gamma((1-nu)/2)*kummerM(z^2/2, -nu/2,1/2)-
+                                           sqrt(2*pi)*z/gamma(-nu/2)*kummerM(z^2/2, (1-nu)/2,3/2))}
